3fcfbfe87fa2bf16cb80414ecf990fbab6bb8142
Rajiv Makhijani <rajiv@blue-tech.org>
2014-11-24 12:24:08 -0800

    sync comments

--- sync.py
+++ sync.py
@@ -50,4 +50,20 @@ def sync_bodies_from_jira():
                 if jissue.fields.description:
                     body += "\n\n" + jissue.fields.description.replace("\r\n", "\n")
                 ghissue.edit(body=body)
-sync_bodies_from_jira()
+
+def sync_comments_from_jira():
+    for ghissue in github_issues:
+        if ghissue.comments == 0:
+            jissue = jira_from_github_issue(ghissue)
+            if jissue is not None:
+                jissue = jira.issue(jissue.key)
+                if jissue.fields.comment:
+                    for jcomment in jissue.fields.comment.comments:
+                        body = "\nComment by: %s" % jira_to_github_author_tag(jcomment.author.name)
+                        body += " on: %s" % jcomment.created
+                        body += "\n\n" + jcomment.body.replace("\r\n", "\n")
+                        print body
+                        ghissue.create_comment(body)
+
+#sync_bodies_from_jira()
+sync_comments_from_jira()
