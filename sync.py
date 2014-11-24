06d25c5c924ebc1b2dcffd4a4ab64e4bb52d4b6f
Rajiv Makhijani <rajiv@blue-tech.org>
2014-11-24 12:13:44 -0800

    sync bodies

--- sync.py
+++ sync.py
@@ -1,3 +1,5 @@
+from __future__ import unicode_literals
+import github3
 from jira.client import JIRA
 from jira.client import GreenHopper
 import json
@@ -7,15 +9,45 @@ config = json.load(open("config.json"))
 options = {
     'server': config['jira']['url']
 }
+jira_prefix = config['jira']['prefix']
 jira = JIRA(options=options, basic_auth=(config['jira']['username'], config['jira']['password']))
-gh = GreenHopper(options=options, basic_auth=(config['jira']['username'], config['jira']['password']))
+green = GreenHopper(options=options, basic_auth=(config['jira']['username'], config['jira']['password']))
 
-boards = gh.boards()
+boards = green.boards()
 
 for board in boards:
-    sprints = gh.sprints(board.id)
+    sprints = green.sprints(board.id)
     print board, sprints
 
 
-issues = jira.search_issues('project=AH AND status != Closed', maxResults=False)
-print len(issues)
+jira_issues = jira.search_issues('project=AH AND status != Closed', maxResults=False)
+jira_issues_by_key = dict((i.key, i) for i in jira_issues)
+
+github = github3.login(config['github']['auth']['username'], config['github']['auth']['password'])
+github_issues = [result.issue for result in github.search_issues("repo:activityhero/kidzexcel state:open")]
+
+def jira_from_github_issue(ghissue):
+    if ghissue.title.startswith(jira_prefix + '-'):
+        key = ghissue.title.split(":")[0]
+        return jira_issues_by_key.get(key)
+
+
+def jira_to_github_author_tag(jira_user):
+    github_user = config['userMapping'].get(jira_user)
+    if github_user:
+        return "@" + github_user
+    else:
+        return "jira: " + jira_user
+
+def sync_bodies_from_jira():
+    for ghissue in github_issues:
+        if len(ghissue.body_html) == 0:
+            jissue = jira_from_github_issue(ghissue)
+            if jissue is not None:
+                body = "%s/browse/%s" % (config['jira']['url'], jissue.key)
+                body += "\nCreated by: %s" % jira_to_github_author_tag(jissue.fields.reporter.name)
+                body += "\nReported on: %s" % jissue.fields.created
+                if jissue.fields.description:
+                    body += "\n\n" + jissue.fields.description.replace("\r\n", "\n")
+                ghissue.edit(body=body)
+sync_bodies_from_jira()
